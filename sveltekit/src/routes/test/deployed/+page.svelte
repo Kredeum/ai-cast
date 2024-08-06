c42c9e8b42b3b1a3a32b58d2a01880a8fdfaa834
zapaz <4560471+zapaz@users.noreply.github.com>
2024-08-06 16:24:17 +0200

    Initial commit

---
+++ sveltekit/src/routes/test/deployed/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+	import deployedContracts from "$lib/contracts/deployedContracts";
+
+	console.log("deployedContracts:", deployedContracts);
+</script>
